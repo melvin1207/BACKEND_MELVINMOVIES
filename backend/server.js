ad0024b721aaa8ffdacc5b232a3302b609e5fa08
Melvin <melsava1207@gmail.com>
2024-03-14 19:16:43 -0600

    conexion con mongodb terminada

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const colors = require('colors')
+const connectDB = require('./config/db')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const cors = require('cors')
 const port = process.env.PORT || 5000
+
+connectDB()
 
 const app = express()
 
@@ -9,5 +14,7 @@ app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({  extended: false }))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Servidor inciado en el puerto ${port}`))

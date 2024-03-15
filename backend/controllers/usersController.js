769e0364779673b60889f9e4d84fc0146c6fe70e
Melvin <melsava1207@gmail.com>
2024-03-15 07:55:33 -0600

    userControllers, creado, falta agregar las funcionalidades

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,41 @@
+const asynHandñer = require('express-async-handler')
+const User = require('../models/usersModel')
+
+//Crear un usuario
+const createUser = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: 'Create user' })
+})
+
+//Logear al usuario
+const loginUser = asyncHandler(async(req,res) => {
+  res.status(200).json({ message: 'Login user' })
+})
+
+//Obtener datos del usuario
+const datosUser = asyncHandler(async(req, res) =>{
+  res.status(200).json({ message: 'get user' })
+})
+
+//Actualizar un usuario
+const updateUser = asyncHandler(async(req, res) =>{
+  res.status(200).json({ message: 'update user' })
+})
+
+//SoftDelete de un usuario
+const softDeleteUser = asyncHandler(async(req, res) =>{
+  res.status(200).json({ message: 'soft delete user' })
+})
+
+//Borrar definitivamente un usuario
+const destroyUser = asyncHandler(async(req, res) =>{
+  res.status(200).json({ message: 'destroy user' })
+})
+
+module.exports = {
+  createUser,
+  loginUser,
+  datosUser,
+  updateUser,
+  softDeleteUser,
+  destroyUser
+}

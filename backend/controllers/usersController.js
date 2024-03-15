eeac624ae09f7f31c70a1cdba73d01083191ec19
Melvin <melsava1207@gmail.com>
2024-03-15 08:58:01 -0600

    usersModel actualizado, y funcion crearUser terminada

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,9 +1,54 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/usersModel')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const { use } = require('../routes/usersRoutes')
 
 //Crear un usuario
 const createUser = asyncHandler(async(req, res) => {
-  res.status(200).json({ message: 'Create user' })
+  //Se destructura el body de la petición
+  const { first_name, last_name, email, password, phone, isAdmin } = req.body
+
+  //Se verifica que la petición tenga todos los datos necesrios
+  if(!first_name || !last_name || !email || !password || !phone || !isAdmin){
+    res.status(400)
+    throw new Error('Faltan datos')
+  }
+
+  //Se verifica que el emial ingresado sea unico
+  const userExist = await User.findOne({ email })
+  if (userExist){
+    res.status(400)
+    throw new Error('El correo electronico ya existe')
+  }
+
+  //Se hace el HASH del password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Objeto para crear al usuario
+  const user = await User.create({
+    first_name,
+    last_name,
+    email,
+    password: hashedPassword,
+    phone,
+    isAdmin
+  }) 
+
+  if(user){
+    res.status(201).json({
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      phone: user.phone,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else{
+    res.status(400)
+    throw new Error('No se pudieron guardar los datos')
+  }
 })
 
 //Logear al usuario
--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -3,15 +3,21 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
   first_name:{
     type: String,
-    required: [true, 'Por favor ingrese el nombre']
+    required: [true, 'Por favor ingrese el nombre'],
+    maxlength : [ 50, 'El nombre no puede exceder los 50 caracteres'],
+    minlength : [ 3, 'El nombre debe contener 3 o más caracteres'] 
   },
   last_name:{
     type: String,
-    required: [true, 'Por favor ingresa el apellido']
+    required: [true, 'Por favor ingresa el apellido'],
+    maxlength : [ 100, 'Los apellidos no pueden exceder los 100 caracteres'],
+    minlength : [ 3, 'El apellido debe contener 3 o más caracteres'] 
   },
   email:{
     type: String,
-    required: [true, 'Ingresa el email del comprador']
+    required: [true, 'Ingresa el email del comprador'],
+    unique: [ true, 'El correo está duplicado'],
+    maxlength: [ 100, 'El correo no puede exceder los 100 caracteres'],
   },
   password:{
     type: String,
@@ -19,7 +25,9 @@ const userSchema = mongoose.Schema({
   },
   phone:{
     type: Number,
-    required: [true, 'Ingresa el numero de telefono']
+    required: [true, 'Ingresa el numero de telefono'],
+    unique: [ true, 'El telefono está duplicado'],
+    maxlength : [ 12, 'El telefono no puede exceder los 12 caracteres']
   },
   isAdmin:{
     type: Boolean,
